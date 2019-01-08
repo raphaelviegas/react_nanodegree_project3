a424142b7260a623519c75383a6efb2ea675468c
Raphael Viegas <raphaelhviegas@gmail.com>
2019-01-08 16:31:36 -0200

    AddDeck form validation prevent duplication of deck names

--- application/screens/main/AddDeck.js
+++ application/screens/main/AddDeck.js
@@ -5,6 +5,7 @@ import PageTitle from '../../components/PageTitle'
 import CustomInput from '../../components/CustomInput'
 import CustomButton from '../../components/CustomButton'
 import { headerFontColor, defaultBackgroundColor, defaultFontColor } from '../../config/colors'
+import { Alert } from 'react-native'
 
 class AddDeck extends React.Component {
 
@@ -31,24 +32,37 @@ class AddDeck extends React.Component {
                 title: deckName,
                 description: deckDescription
             }
-            //Call action
-            this.props.store.fetchAddDeck({ key: deckName, deckInfo }).then(() => {
-                //Go to Deck Page
-                this.props.navigation.navigate('Deck', { key: deckName })
-            })
+            if (this._checkDeckKey(deckInfo)) {
+                //Call action
+                this.props.store.fetchAddDeck({ key: deckName, deckInfo }).then(() => {
+                    //Go to Deck Page
+                    this.props.navigation.navigate('Deck', { key: deckName })
+                })
 
-            //Clear state
-            this.setState(() => ({
-                deckName: '',
-                deckDescription: '',
-                showErrorMessage: false
-            }))
+                //Clear state
+                this.setState(() => ({
+                    deckName: '',
+                    deckDescription: '',
+                    showErrorMessage: false
+                }))
+            } else {
+                Alert.alert('Deck already exists', 'This deck name already exists. Please select another value.')
+            }
         }
+    }
 
+    _checkDeckKey = (deckInfo) => {
+        const auxiliar = this.props.store.decks.filter(deck => deck.title === deckInfo.title).length
+        return (auxiliar === 0)
     }
 
     render() {
         const { deckName, deckDescription, showErrorMessage } = this.state
+        const deckInfo = {
+            title: deckName,
+            description: deckDescription
+        }
+        this._checkDeckKey(deckInfo)
         return (
             <BasicView justifyContent="center">
                 <PageTitle headerFontColor={headerFontColor}>Add new deck</PageTitle>
